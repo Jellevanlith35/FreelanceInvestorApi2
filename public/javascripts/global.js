906db5cc9b06760002ae8a8538e91fb5fe75cca3
jellevanlith35 <Jelle_van_lith@hotmail.com>
2016-09-08 16:27:00 +0200

    jade

---
+++ public/javascripts/global.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var login = {
+    emailaddress: "jelle_van2_lith@hotmail.com",
+    password: "test12345"
+  }
+  $.ajax({
+          type: 'POST',
+          data: login,
+          url: '/accounts/validate/login',
+          dataType: 'JSON'
+      }).done(function (response) {
+          console.log(response);
+      });
+});
