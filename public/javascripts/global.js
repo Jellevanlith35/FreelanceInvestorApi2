a36df8b4ed4fa3c744a4c53f90f561b21065c52c
jellevanlith35 <Jelle_van_lith@hotmail.com>
2016-09-08 17:03:58 +0200

    Global

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
-  var login = {
-    emailaddress: "jelle_van2_lith@hotmail.com",
-    password: "test12345"
-  }
-  $.ajax({
-          type: 'POST',
-          data: login,
-          url: 'api/accounts/validate/login',
-          dataType: 'JSON'
-      }).done(function (response) {
-          console.log(response);
-      });
-});
+// $(document).ready(function() {
+//   var login = {
+//     emailaddress: "jelle_van2_lith@hotmail.com",
+//     password: "test12345"
+//   }
+//   $.ajax({
+//           type: 'POST',
+//           data: login,
+//           url: 'api/accounts/validate/login',
+//           dataType: 'JSON'
+//       }).done(function (response) {
+//           console.log(response);
+//       });
+// });
