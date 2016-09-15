7d6cbec9c547e4eb5b7d4da3f0b172bc3ca10954
jellevanlith35 <Jelle_van_lith@hotmail.com>
2016-09-15 22:17:29 +0200

    Api

--- models/accountJob.js
+++ models/accountJob.js
@@ -5,8 +5,10 @@ relationship = require("mongoose-relationship");
 console.log('Initializing account job schema');
 
 var AccountJob = new Schema({
-    account: { type: Schema.ObjectId, ref: 'Account' },
-    job: { type: Schema.ObjectId, ref: 'Job' },
+    // account: { type: Schema.ObjectId, ref: 'Account' },
+    // job: { type: Schema.ObjectId, ref: 'Job' },
+    account: {type: String, required: false},
+    job: {type: String, required: false}
 }, { collection : 'accountJobs' });
 
 module.exports = mongoose.model('AccountJob', AccountJob);
